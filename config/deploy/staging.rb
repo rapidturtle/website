8720cb68fcdd893ae24227cc01b8702a5d620ec3
Andrew Porter <partydrone@me.com>
2011-09-09 06:20:49 -0600

    Temporary workaround for precompiling assets
    When the Gemfile has been updated the rake task to
    precompile the assets will fail because
    bundle:install has not been run yet.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -22,4 +22,7 @@ namespace :deploy do
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
+  
+  # Temporary workaround for precompiling assets
+  before "deploy:assets:precompile", "bundle:install"
 end
