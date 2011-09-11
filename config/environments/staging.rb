722cac6cbcbe23470beedd58968a5681cb255174
Andrew Porter <partydrone@me.com>
2011-09-11 17:25:42 -0600

    Modify environment to be more appropriate for staging.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -4,9 +4,9 @@ Rapidturtle::Application.configure do
   # Code is not reloaded between requests
   config.cache_classes = true
 
-  # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = false
+  # Full error reports are enabled and caching is turned on
+  config.consider_all_requests_local       = true
+  config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
@@ -25,7 +25,7 @@ Rapidturtle::Application.configure do
 
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
