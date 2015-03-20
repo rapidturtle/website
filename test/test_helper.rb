2220b9f486b8e7723153bc06b107a242d7308b88
Andrew Porter <andrew.porter@wavetronix.com>
2015-03-20 01:14:02 -0600

    Update test environment configuration

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,9 +2,27 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+require 'database_cleaner'
+require 'minitest/rails'
+require 'minitest/rails/capybara'
+
+Dir[File.expand_path('test/factories/**/*.rb')].each { |file| require file }
+
+DatabaseCleaner.strategy = :transaction
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+end
+
+class Minitest::Spec
+  before do
+    DatabaseCleaner.start
+  end
+
+  after do
+    DatabaseCleaner.clean
+  end
 end
