aff096070706f716b9e928edcb935cd51445233a
Andrew Porter <andrew.porter@wavetronix.com>
2015-03-19 22:20:57 -0600

    Create a blank Rails 4.2 app

--- config/initializers/mail_server.rb
+++
@@ -1,3 +0,0 @@
-# Configure mail delivery options for Amazon Simple Email Service
-
-# RapidTurtle::Application.config.action_mailer.delivery_method = :amazon_ses
--- spec/spec_helper.rb
+++
@@ -1,8 +0,0 @@
-ENV["RAILS_ENV"] = "test"
-require File.expand_path("../../config/environment", __FILE__)
-require 'minitest/autorun'
- 
-Turn.config do |t|
-  t.format = :outline
-  t.natural = true
-end
