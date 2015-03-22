80b760f1c6f69e2cd284af6ea0b58fb259c08c8b
Andrew Porter <aporter@rapidturtle.com>
2015-03-21 22:43:15 -0600

    Move home page to static_pages controller
    Keep it all together.

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 describe StaticPagesController do
+  it "gets home" do
+    get :home
+    must_respond_with :success
+  end
+
   it "gets contact" do
     get :contact
     must_respond_with :success
